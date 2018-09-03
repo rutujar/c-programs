e84063cdf3bb12387ac1d2e598b6b4d9b4597e1c
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-09-03 22:19:43 +0530

    Create swapwithouttemp.cs

---
+++ csharp/swapwithouttemp.cs
@@ -0,0 +1,19 @@
+using System;
+public class swap
+{
+       public static void Main(string[] args)
+         {
+            int number1, number2;
+            Console.Write("\nInput the First Number : ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the Second Number : ");
+            number2 = int.Parse(Console.ReadLine());
+            number1=number1+number2;
+            number2=number1-number2;
+            number1=number1-number2;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : "+number1);
+            Console.Write("\nSecond Number : "+number2);
+            Console.Read();
+        }
+}
