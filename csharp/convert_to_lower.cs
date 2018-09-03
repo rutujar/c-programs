8d5671809a83d8237fde7309dd9d5663083d3703
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-09-03 23:13:30 +0530

    Create convert_to_lower.cs

---
+++ csharp/convert_to_lower.cs
@@ -0,0 +1,9 @@
+using System;  
+public class example
+{  
+    public static void Main() 
+      {          
+                    string line = "Write a C# Sharp Program to display the following pattern using the alphabet."; 
+                        Console.WriteLine(line.ToLower());
+    }
+}
