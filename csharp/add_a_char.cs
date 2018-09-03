43fed84f85b88fcdb834438e2a384e83e1b8dd36
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-09-03 22:53:23 +0530

    Create add_a_char.cs

---
+++ csharp/add_a_char.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+public class example {
+  static void Main(string[] args)
+        {
+           string str; 
+           Console.Write("Input a string : ");
+           str = Console.ReadLine();
+           if (str.Length>=1)
+           {
+           var s = str.Substring(0,1);
+           Console.WriteLine(s + str + s);
+           }
+        }
+}
