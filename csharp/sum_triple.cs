3d280a91bc1c0b13f0edfef141196971fdcf3f07
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-09-03 23:10:11 +0530

    Create sum_triple.cs

---
+++ csharp/sum_triple.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+public class example {
+  static void Main(string[] args)
+        {
+            Console.WriteLine(SumTriple(2,2));
+            Console.WriteLine(SumTriple(12,10));
+            Console.WriteLine(SumTriple(-5,2));          
+        }
+        public static int SumTriple(int a, int b)
+        {
+            return a == b ? (a + b)*3 : a + b;
+        }
+}
