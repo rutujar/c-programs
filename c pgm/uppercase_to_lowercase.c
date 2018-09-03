2032f65c6ebed94bd7cd64a39f0e6908d4f52960
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-09-03 23:07:49 +0530

    Create uppercase_to_lowercase.c

---
+++ c pgm/uppercase_to_lowercase.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	char ch;
+	printf("Enter a character in uppercase : ");
+	scanf("%c",&ch);
+	ch=ch+32;
+	printf("character in lowercase = %c",ch);
+	getch();
+}
