d2bc2c3d97137e1cdd25ba6de60f22c2a7154514
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-09-03 23:09:23 +0530

    Create check_pos_neg.cs

---
+++ csharp/check_pos_neg.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+public class example {
+  static void Main(string[] args)
+        {
+           Console.WriteLine("\nInput first integer:");  
+           int x = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Input second integer:");  
+           int y = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Check if one is negative and one is positive:");
+           Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
+        }
+}
