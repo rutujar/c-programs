c2b5a43171172eba8680b48f63866c85b94f820a
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-09-03 22:44:37 +0530

    Create temp.cs

---
+++ csharp/temp.cs
@@ -0,0 +1,11 @@
+using System;
+public class temp
+{
+   public static void Main( )
+    {
+        Console.Write("Enter the amount of celsius: "); 
+        int celsius = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Kelvin = {0}", celsius + 273);
+        Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+    }
+}
