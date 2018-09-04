72c2133b3ca0a4154a970d98e9168fe6092535da
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-09-04 17:53:13 +0530

    Create cube.java

---
+++ java/cube.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int x;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("cube is");
+		x=sc.nextInt();
+		x=x*x*x;
+		System.out.println(x);
+	}
+}
