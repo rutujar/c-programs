b0d538a6138bde554957dd82ba334d28d7e04974
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-09-04 17:56:26 +0530

    Create sqroot.java

---
+++ java/sqroot.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		double x,d;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("square root is");
+		x=sc.nextDouble();
+	d = Math.sqrt(x);
+		System.out.println(d);
+	}
+}
